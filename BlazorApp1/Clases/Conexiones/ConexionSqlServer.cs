353fd2b58390027cac5989d3523732d4a9a7e1eb
Juan Jesus Guzman Mata <juanguz97@hotmail.com>
2022-03-24 21:50:51 -0500

    Agregar archivos de proyecto.

---
+++ BlazorApp1/Clases/Conexiones/ConexionSqlServer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace BlazorApp1.Clases.Conexiones
+{
+    public class ConexionSqlServer
+    {
+        public void Conexion()
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "<your_server>.database.windows.net";
+                builder.UserID = "<your_username>";
+                builder.Password = "<your_password>";
+                builder.InitialCatalog = "<your_database>";
+
+                using (SqlConnection connection = new SqlConnection(builder.ConnectionString))
+                {
+                    Console.WriteLine("\nQuery data example:");
+                    Console.WriteLine("=========================================\n");
+
+                    String sql = "SELECT name, collation_name FROM sys.databases";
+
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        connection.Open();
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0} {1}", reader.GetString(0), reader.GetString(1));
+                            }
+                        }
+                    }
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
